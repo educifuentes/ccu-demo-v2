e98df6418f9845a138a46230a81430b2324cf006
educifuentes <edu.cifuentes@gmail.com>
2026-01-12 00:54:09 -0300

    refactor: simplify classification color mapping in config
    - Removed the "Sin comodato o terminado" classification from the color mapping in utils/config.py for clarity and conciseness.

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,5 @@
 CLASIFICACION_COLORS = {
     "En regla": "#83c9ff", 
     "No en regla": "#ffabab",
-    "No aplica": "#CBDCEB",
-    "Sin comodato o terminado": "#9F8383"
+    "No aplica": "#CBDCEB"
 }
